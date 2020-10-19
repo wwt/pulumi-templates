89f6ccb8fe9d67c0db450424de669a7320c0e45b
Brian Koehler <brian.koehler@wwt.com>
2020-10-19 13:04:15 -0600

    Additional comments; remove all hard coded values

--- aws-eks-infra/index.ts
+++ aws-eks-infra/index.ts
@@ -3,24 +3,26 @@ import * as awsx from "@pulumi/awsx";
 import * as pulumi from "@pulumi/pulumi";
 import * as eks from "@pulumi/eks";
 
-// Get configuration for the stack
+// Get the configuration for the current stack
 const config = new pulumi.Config();
 
+// Create the baseTags object
 const baseTags = {
     Owner: config.require("ownerTag")
 };
 
-// Create a VPC for the cluster.
-const vpc = new awsx.ec2.Vpc("kafka-vpc", {
+// Create a VPC for the cluster with configured number of availability zones
+// and named with the provided name.
+const vpc = new awsx.ec2.Vpc(config.require("vpcName"), {
     numberOfAvailabilityZones: config.requireNumber("numOfAvailZones"),
     tags: {
         ...baseTags,
-        Name: "kafka-vpc"
+        Name: config.require("vpcName")
     }
 });
 
-// Create an EKS cluster with the given configuration.
-const cluster = new eks.Cluster("kafka-cluster", {
+// Create an EKS cluster with the provided configuration options.
+const cluster = new eks.Cluster(config.require("eksClusterName"), {
     vpcId: vpc.id,
     subnetIds: vpc.privateSubnetIds,
     instanceType: config.require("instanceType") as aws.ec2.InstanceType,
@@ -30,13 +32,14 @@ const cluster = new eks.Cluster("kafka-cluster", {
     tags: baseTags
 });
 
-// Export kubeconfig
+// Export the kubeconfig
 export const kubeconfig = cluster.kubeconfig;
 
-// Export the vpc zones for use in the confluent operator aws provider config
+// Export the vpc zones for use in the confluent operator project
 export const vpcZones = vpc.privateSubnetIds.then(subnetIds =>
     subnetIds.map(id => id.apply(
         id => aws.ec2.getSubnet({id}).then(result =>
-            result.availabilityZone)
+            result.availabilityZone
+        )
     ))
 );
